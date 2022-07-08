63f3ba106b14245ca51cbb3024a8209d1b34d8da
unknown <shaiksyed7860@gmail.com>
2022-07-08 19:12:27 +0530

    changes added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View, ImageBackground, Component} from 'react'
 import Cookies from 'js-cookie'
 import {Redirect, Link} from 'react-router-dom'
 
-import Loader from 'react-loader-spinner'
+import * as Loader from 'react-loader-spinner'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
 import Header from '../Header'
@@ -68,10 +68,10 @@ class Home extends Component {
   }
 
   render() {
-    const jwtToken = Cookies.get('jwt_token')
-    if (jwtToken === undefined) {
-      return <Redirect to="/login" />
-    }
+    // const jwtToken = Cookies.get('jwt_token')
+    // if (jwtToken === undefined) {
+    //   return <Redirect to="/login" />
+    // }
 
     const {trendingData, topData, isLoading, movieData} = this.state
 
